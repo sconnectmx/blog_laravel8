28fe9d27f3d9bc921e5ac26d724bb5168ccb03a9
osanamc <osanmac@gmai.com>
2021-03-30 00:23:53 -0600

    Carga Inicial

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title','Crear Curso')
+
+@section('content')
+<h1>En esta página podrás crear un curso</h1>
+@endsection
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title','Principal Cursos')
+
+@section('content')
+<h1>Bienvenido a la pagina de Cursos</h1>
+@endsection
